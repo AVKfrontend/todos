7bfed625b9765c1889873b01bf91715bdd90aebd
AVK <aleks_vk@ukr.net>
2023-08-14 15:27:40 +0300

    Start screen
    Signed-off-by: AVK <aleks_vk@ukr.net>

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <template>
   <main>
+    <div class="app-wrapper">
+      <div class="app-body">
 
+        <StartView v-if="isTodoListEmpty" @start="setFirstTask" />
+
+      </div>
+
+      <footer class="app-footer">
+        Criated by Vue on Vite
+      </footer>
+    </div>
   </main>
 </template>
 
 <script>
+import StartView from './components/StartView.vue';
+
+
+export default {
+  components: { StartView },
+  data() {
+    return {
+      todoList: []
+    };
+  },
+  computed: {
+    isTodoListEmpty() { return !this.todoList.length; }
+  },
+  methods: {
+    setFirstTask() {
+      console.log('set task')
+    }
+  }
+}
+
+
 </script>
 
-<style></style>
---
+++ src/components/StartView.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="app-title">Планування</h1>
+  <button @click="$emit('start')" class="app-btn app-btn--start" type="button">почати</button>
+</template>
+
+<script>
+export default {
+  emits: {
+    start: null
+  }
+}
+</script>
